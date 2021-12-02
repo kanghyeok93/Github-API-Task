2529de877afc92d03aabb627f9809291be1fbd5c
SeoKangHyeok <kanghyeok93@naver.com>
2021-12-03 02:01:54 +0900

    feat<eslintrc.js>: rules add

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,17 @@ module.exports = {
     'airbnb/hooks',
     'plugin:prettier/recommended',
   ],
-  rules: {},
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'func-names': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
 };
