1af38053e03408a96a61c891817052a38280d8de
SeoKangHyeok <kanghyeok93@naver.com>
2021-12-10 22:30:41 +0900

    feat: npm install redux-devtools-extension && setting
    setting global store redux-devtools-extension

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import * as modules from './modules';
 import rootSaga from './modules/rootSaga';
@@ -14,7 +15,7 @@ middlewares.push(sagaMiddleware);
 
 // 개발 모드일 때만 Redux Devtools 적용
 const isDev = process.env.NODE_ENV === 'development';
-const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const devtools = isDev && composeWithDevTools;
 const composeEnhancers = devtools || compose;
 
 const configure = preloadedState => {
